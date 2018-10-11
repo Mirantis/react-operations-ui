468569d198c99d8531d6002a27495d17a9babd40
Ekaterina Chernova <efedorova@mirantis.com>
2018-10-11 18:53:19 +0300

    Initial commit

---
+++ operations-ui/src/Table.js
@@ -0,0 +1,42 @@
+const ReactDataGrid = require('react-data-grid');
+const React = require('react');
+
+class Example extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.createRows();
+    this._columns = [
+      { key: 'id', name: 'ID' },
+      { key: 'title', name: 'Title' },
+      ];
+
+    this.state = null;
+  }
+
+  createRows = () => {
+    let rows = [];
+    for (let i = 1; i < 1000; i++) {
+      rows.push({
+        id: i,
+        title: 'Title ' + i
+      });
+    }
+
+    this._rows = rows;
+  };
+
+  rowGetter = (i) => {
+    return this._rows[i];
+  };
+
+  render() {
+    return  (
+      <ReactDataGrid
+        columns={this._columns}
+        rowGetter={this.rowGetter}
+        rowsCount={this._rows.length}
+        minHeight={500} />);
+  }
+}
+
+export default Example;
