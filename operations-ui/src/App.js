ce736920ce1069d8bdb01caf643db7c15c062d8b
Ekaterina Chernova <efedorova@mirantis.com>
2018-10-17 16:14:12 +0300

    Open wizard on button click

--- operations-ui/src/App.js
+++ operations-ui/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Header from "./Header";
 import TemplatesTable from "./TemplatesTable";
-import ReclassModelWizard from "./ReclassModelWizard";
-
 
 import './App.css';
 
@@ -10,15 +8,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-
       <div>
-
         <Header />
-        {/*<ReclassModelWizard />*/}
         <TemplatesTable />
       </div>
-
-
     );
   }
 }
