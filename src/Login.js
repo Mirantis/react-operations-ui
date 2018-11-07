9e57740fff3cca8e167712f4452636fedc2ef6d4
Ekaterina Chernova <efedorova@mirantis.com>
2018-11-07 16:23:18 +0300

    Redirect to login page when token is expired

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Form, Button, Input, FormGroup} from 'reactstrap';
 import axios from "axios";
 
+import AuthContext from './App'
+
 class Login extends Component {
   constructor(props) {
     super(props);
